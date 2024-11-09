622c895586b74eb5f4c4fedc9db0440f9f61b757
MiracleChiji <mchiji905@gmail.com>
2024-11-09 18:13:51 +0100

    remove empty practice

--- practice_1/target/debug/deps/practice_1-16de299a33924e5e.d
+++
@@ -1,5 +0,0 @@
-/Users/miraclechiji/Documents/m.chijiCOS101/practice_1/target/debug/deps/libpractice_1-16de299a33924e5e.rmeta: src/main.rs
-
-/Users/miraclechiji/Documents/m.chijiCOS101/practice_1/target/debug/deps/practice_1-16de299a33924e5e.d: src/main.rs
-
-src/main.rs:
--- practice_1/target/debug/deps/practice_1-683f5af4c254eb85.d
+++
@@ -1,5 +0,0 @@
-/Users/miraclechiji/Documents/m.chijiCOS101/practice_1/target/debug/deps/libpractice_1-683f5af4c254eb85.rmeta: src/main.rs
-
-/Users/miraclechiji/Documents/m.chijiCOS101/practice_1/target/debug/deps/practice_1-683f5af4c254eb85.d: src/main.rs
-
-src/main.rs:
